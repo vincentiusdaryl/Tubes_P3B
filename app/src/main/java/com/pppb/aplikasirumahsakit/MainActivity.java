d1e6bb71d5c71e8e6612759603a99c6f1abf2786
vincentiusdaryl <6181901023@student.unpar.ac.id>
2022-10-20 17:59:53 +0700

    add activity main binding

--- app/src/main/java/com/pppb/aplikasirumahsakit/MainActivity.java
+++ app/src/main/java/com/pppb/aplikasirumahsakit/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.pppb.aplikasirumahsakit.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
