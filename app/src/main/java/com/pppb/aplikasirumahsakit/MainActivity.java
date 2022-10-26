7c843760b975e72fcc32cc760b5d2ec309dcd7ac
Ranks007 <Michaelalex2705@gmail.com>
2022-10-26 07:25:16 +0700

    fragment_home java

--- app/src/main/java/com/pppb/aplikasirumahsakit/MainActivity.java
+++ app/src/main/java/com/pppb/aplikasirumahsakit/MainActivity.java
@@ -1,12 +1,17 @@
 package com.pppb.aplikasirumahsakit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 import com.pppb.aplikasirumahsakit.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    protected fragment_home fragmentHome;
+    protected FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/pppb/aplikasirumahsakit/fragment_home.java
+++ app/src/main/java/com/pppb/aplikasirumahsakit/fragment_home.java
@@ -1,7 +1,54 @@
 package com.pppb.aplikasirumahsakit;
 
-public class fragment_home {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+
+public class fragment_home extends Fragment {
+
+    private FragmentManager fragmentManager;
     public fragment_home(){
 
     }
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        this.getParentFragmentManager().setFragmentResultListener("msg", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String msg = result.getString("msg");
+                setMessage(msg);
+            }
+        });
+
+        return view;
+    }
+
+    public static fragment_home newInstance(String title) {
+        fragment_home fragment = new fragment_home();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void onClick(View view) {
+        Bundle result = new Bundle();
+        result.putInt("page", 2);
+        this.getParentFragmentManager().setFragmentResult("changePage", result);
+    }
+
+    public void setMessage(String msg) {
+
+    }
+
 }
