fb0936eaf843c40c23f6591144d56ef33443d666
vincentiusdaryl <6181901023@student.unpar.ac.id>
2022-11-01 21:19:33 +0700

    buat_janji.xml tampilannya udah beres tinggal java nya. fragment_list_dokter.xml udah ditambahin tombolnya pake Floating Action Button.

--- app/src/main/java/com/pppb/aplikasirumahsakit/BuatJanji.java
+++ app/src/main/java/com/pppb/aplikasirumahsakit/BuatJanji.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -16,22 +17,29 @@ import java.util.*;
 public class BuatJanji extends AppCompatActivity {
 
     EditText etDate;
+    EditText etTime;
     int day;
     int month;
     int year;
+    int hour;
+    int minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buat_janji);
         etDate = findViewById(R.id.etDate);
+        etTime = findViewById(R.id.etTime);
+
         final Calendar calendar = Calendar.getInstance();
+
         etDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 day = calendar.get(Calendar.DAY_OF_MONTH);
                 month = calendar.get(Calendar.MONTH);
                 year = calendar.get(Calendar.YEAR);
+
                 DatePickerDialog datePickerDialog = new DatePickerDialog(BuatJanji.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int day, int month, int year) {
@@ -39,6 +47,22 @@ public class BuatJanji extends AppCompatActivity {
                     }
                 }, year, month, day);
                 datePickerDialog.show();
+            }
+        });
+
+        etTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                hour = calendar.get(Calendar.HOUR_OF_DAY);
+                minute = calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(BuatJanji.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        etTime.setText(hourOfDay + ":" + minute);
+                    }
+                }, hour, minute, true);
+                timePickerDialog.show();
             }
         });
     }
