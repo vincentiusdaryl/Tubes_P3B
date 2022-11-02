16fa7e258d2843d163a981c5ed955d488c1f7802
Ranks007 <Michaelalex2705@gmail.com>
2022-11-02 14:20:14 +0700

    bikin db fragment tambah dokter sama xml

--- app/src/main/java/com/pppb/aplikasirumahsakit/fragment_tambahdokter.java
+++ app/src/main/java/com/pppb/aplikasirumahsakit/fragment_tambahdokter.java
@@ -1,16 +1,20 @@
 package com.pppb.aplikasirumahsakit;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import com.pppb.aplikasirumahsakit.databinding.FragmentTambahdokterBinding;
 
 public class fragment_tambahdokter extends Fragment {
-FragmentTambahdokterBinding binding;
 
+FragmentTambahdokterBinding binding;
+databaseDokter db;
     public fragment_tambahdokter(){
 
     }
@@ -19,8 +23,19 @@ FragmentTambahdokterBinding binding;
         return fragment;
     }
 
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         binding = FragmentTambahdokterBinding.inflate(inflater);
+        db = new databaseDokter(requireContext());
 
+        binding.add.setOnClickListener(this::onClick);
+    }
+    public void onClick(View view){
+        String name = binding.etNama.getText().toString();
+        String spesialis = binding.etSpesialis.getText().toString();
+
+        if(!name.equals("")&&!spesialis.equals("")&&db.insertData(name,spesialis)){
+            Toast.makeText(getContext(), "Data saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
