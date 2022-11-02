97a29cc1ce848ee8260961fac2f3efbd22883dbb
vincentiusdaryl <6181901023@student.unpar.ac.id>
2022-11-02 20:52:59 +0700

    Ngebenerin list_dokter.xml sama buat fragment_list_janji.xml dan list_janji.xml

---
+++ app/src/main/java/com/pppb/aplikasirumahsakit/ListJanji.java
@@ -0,0 +1,27 @@
+package com.pppb.aplikasirumahsakit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListJanji extends Fragment {
+
+    public ListJanji() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
+}
